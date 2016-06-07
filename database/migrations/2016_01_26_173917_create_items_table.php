aa482581de4bde35116b9a4eb0eb683190962234
Jimmy Ho <jho@twilio.com>
2016-06-06 21:32:28 -0700

    updated database

--- database/migrations/2016_01_26_173917_create_items_table.php
+++ database/migrations/2016_01_26_173917_create_items_table.php
@@ -20,8 +20,11 @@ class CreateItemsTable extends Migration
             $table->string('summary', 1024)->nullable();
             $table->string('description', 2048)->nullable();
             $table->integer('quantity')->nullable();
+			// status
             $table->float('price')->nullable();
+			// price_currency
             $table->float('weight')->nullable();
+			// weight_units
             $table->timestamps();
         });
 
@@ -30,7 +33,10 @@ class CreateItemsTable extends Migration
             $table->integer('item_id')->unsigned()->index();
             $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
             $table->string('uri', 1024);
-            $table->string('type', 60);
+            $table->string('content_type', 60);
+			// file size
+			// original name
+			// read only
             $table->timestamps();
         });
 
@@ -63,13 +69,29 @@ class CreateItemsTable extends Migration
         });
 
         Schema::create('item_locations', function (Blueprint $table) {
-            $table->increments('id');
             $table->integer('item_id')->unsigned()->index();
             $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
-            $table->string('tag', 60);
-            $table->string('type', 60);
+            $table->integer('location_id')->unsigned()->index();
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+            $table->string('bin', 60);
+            $table->integer('quantity');			
             $table->timestamps();
         });
+		
+        Schema::create('item_vendors', function (Blueprint $table) {
+            $table->integer('item_id')->unsigned()->index();
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->integer('vendor_id')->unsigned()->index();
+            $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
+            $table->float('price');
+            $table->integer('quantity');
+			$table->integer('ordered');
+			$table->integer('reorder');
+            $table->timestamps();
+        });		
+		
+		// transactions
+		// item customers
     }
 
     /**
